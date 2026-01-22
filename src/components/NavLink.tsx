39d42fd9c17140ee6952ab6c4c4cb8f355b0ceab
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2026-01-22 09:52:07 -0600

    fix: navlink final fix

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -4,9 +4,13 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { forwardRef, type AnchorHTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
+import type { LinkProps } from "next/link";
 
-interface NavLinkCompatProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href" | "className"> {
-  to: string;
+interface NavLinkCompatProps extends Omit<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  "href" | "className"
+> {
+  to: LinkProps["href"];
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
@@ -15,7 +19,7 @@ interface NavLinkCompatProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
   ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
     const pathname = usePathname();
-    const toPath = to;
+    const toPath = typeof to === "string" ? to : (to?.pathname ?? "");
     const isActive = toPath ? pathname === toPath : false;
     const isPending = false;
 
